2d121cf282f032a9d81ef490fa49a33ee034bd7b
Kenta11 <kk.068.kenta@gmail.com>
2017-05-23 16:02:32 +0900

    initial commit

---
+++ kenta/Reversi_AI_Random.h
@@ -0,0 +1,22 @@
+/*! @file
+ @brief ランダムAI
+ @author 新井
+ @date 2017.5.23
+*/
+
+#ifndef REVERSI_AI_RANDOM_H
+#define REVERSI_AI_RANDOM_H
+
+#include "Board.h"
+
+/*! @brief AIのクラス
+*/
+class Reversi_AI_Random{
+  public:
+    Reversi_AI_Random();
+    void return_move(Board b, int flagin, int *x, int *y, int *flagout);
+  private:
+    bool Berserker_Soul;
+};
+
+#endif
